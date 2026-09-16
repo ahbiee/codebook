c14f1cf9b282b1b104cca663353687cbe575d6dc
ahbiee <james35744@gmail.com>
2026-09-16 14:41:01 +0800

    Refactor BIT class methods for clarity and efficiency

--- source/DataStructure/BIT.cpp
+++ source/DataStructure/BIT.cpp
@@ -1,31 +1,31 @@
-struct BIT {
-    int n;
-    vector<long long> tree; // 使用 ll 避免區間總和溢位
+int n; // 數值上界，BIT的範圍從1~n
+vector<long long> tree; // 使用 ll 避免區間總和溢位
 
-    void init(int sz){
-        n = sz;
-        tree.assign(sz+1, 0);
-    }
+void init(int sz){
+    n = sz;
+    tree.assign(sz+1, 0);
+}
 
-    inline int lowbit(int x) { // 取得二進位中最右邊的 1
-        return x & (-x);
-    }
+inline int lowbit(int x) { // 取得二進位中最右邊的 1
+    return x & (-x);
+}
 
-    void add(int x, long long val) { // 在位置 x 加上了 val
-        for (; x <= n; x += lowbit(x)) {
-            tree[x] += val;
-        }
+void add(int x, long long val) { // 在位置 x 加上了 val
+    while(x <= n) {
+        tree[x] += val;
+        x += lowbit(x);
     }
+}
 
-    long long query(int x) { // 計算 [1, x] 的總和
-        long long sum = 0;
-        for (; x > 0; x -= lowbit(x)) {
-            sum += tree[x];
-        }
-        return sum;
+long long query(int x) { // 計算 [1, x] 的總和
+    long long sum = 0;
+    while(x > 0) {
+        sum += tree[x];
+        x -= lowbit(x);
     }
+    return sum;
+}
 
-    long long query(int l, int r) { // 計算 [l, r] 的總和 (前綴和相減)
-        return query(r) - query(l - 1);
-    }
-};
+long long query(int l, int r) { // 計算 [l, r] 的總和 (前綴和相減)
+    return query(r) - query(l - 1);
+}
